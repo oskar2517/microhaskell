0ad483b74c8b5e41afff1fbfb93321f6aeee07d6
Oskar <12718920+oskar2517@users.noreply.github.com>
2025-05-07 17:15:20 +0200

    simplify thunk

--- src/main/java/me/oskar/microhaskell/evaluation/expression/Thunk.java
+++ src/main/java/me/oskar/microhaskell/evaluation/expression/Thunk.java
@@ -4,22 +4,19 @@ import java.util.Map;
 
 public class Thunk implements Expression {
 
-    private Expression expression;
+    private final Expression expression;
     private final Map<String, Expression> env;
-    private Expression value;
-    private boolean evaluated = false;
+    private Expression value = null;
 
     public Thunk(Expression expression, Map<String, Expression> env) {
         this.expression = expression;
         this.env = env;
     }
-
+    
     @Override
     public Expression evaluate(Map<String, Expression> outerEnv) {
-        if (!evaluated) {
+        if (value == null) {
             value = expression.evaluate(env);
-            evaluated = true;
-            expression = null;
         }
         return value;
     }
