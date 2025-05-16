511f090653f0be6a6714a3c1bcd2256212633313
Oskar <12718920+oskar2517@users.noreply.github.com>
2025-05-16 21:04:36 +0200

    Mutually recursive dispatch (#7)
    * implement wip mutually recursive dispatch
    
    * cleanup code
    
    * remove lambdadispatch record
    
    * rename lambdabodies variable
    
    * move dispatcher name to constant
    
    * remove recursive aliases
    
    * move tag name into constant
    
    * fix formatting

--- src/main/java/me/oskar/microhaskell/ir/IRGeneratorVisitor.java
+++ src/main/java/me/oskar/microhaskell/ir/IRGeneratorVisitor.java
@@ -9,7 +9,8 @@ import java.util.*;
 public class IRGeneratorVisitor implements Visitor<Expression> {
 
     private String currentFunctionName = null;
-    private String recursiveAlias = null;
+    private final Map<String, Expression> dispatchedLambdaBodies = new HashMap<>();
+    private Map<String, Integer> dispatchedLambdaIds;
 
     private static final Expression Y_COMBINATOR = new Lambda("f",
             new Application(
@@ -28,11 +29,14 @@ public class IRGeneratorVisitor implements Visitor<Expression> {
             )
     );
 
+    private static final String MUTUAL_DISPATCHER_NAME = "<mutual_dispatch>";
+    private static final String MUTUAL_DISPATCHER_TAG = "<tag>";
+
     @Override
     public Expression visit(AnonymousFunctionNode anonymousFunctionNode) {
         var body = anonymousFunctionNode.getBody().accept(this);
 
-        for (int i = anonymousFunctionNode.getParameters().size() - 1; i >= 0; i--) {
+        for (var i = anonymousFunctionNode.getParameters().size() - 1; i >= 0; i--) {
             var param = (IdentifierNode) anonymousFunctionNode.getParameters().get(i);
             body = new Lambda(param.getName(), body);
         }
@@ -50,13 +54,21 @@ public class IRGeneratorVisitor implements Visitor<Expression> {
 
     @Override
     public Expression visit(FunctionDefinitionNode functionDefinitionNode) {
-        var previousFunctionName = currentFunctionName;
-        var previousAlias = recursiveAlias;
+        if (functionDefinitionNode.isAppliedMutuallyRecursively()) {
+            var body = functionDefinitionNode.getBody().accept(this);
+
+            for (int i = functionDefinitionNode.getParameters().size() - 1; i >= 0; i--) {
+                var param = (IdentifierNode) functionDefinitionNode.getParameters().get(i);
+                body = new Lambda(param.getName(), body);
+            }
+
+            dispatchedLambdaBodies.put(functionDefinitionNode.getName(), body);
+
+            return new Application(new Variable(MUTUAL_DISPATCHER_NAME),
+                    new IntLiteral(functionDefinitionNode.getDispatchId()));
+        }
 
         currentFunctionName = functionDefinitionNode.getName();
-        recursiveAlias = functionDefinitionNode.isAppliedRecursively()
-                ? "__rec_%s".formatted(currentFunctionName)
-                : null;
 
         var body = functionDefinitionNode.getBody().accept(this);
 
@@ -65,14 +77,10 @@ public class IRGeneratorVisitor implements Visitor<Expression> {
             body = new Lambda(param.getName(), body);
         }
 
-        // Wrap in Y combinator if recursive
         if (functionDefinitionNode.isAppliedRecursively()) {
-            body = new Lambda(recursiveAlias, body);
+            body = new Lambda(currentFunctionName, body);
             body = new Application(Y_COMBINATOR, body);
         }
-
-        currentFunctionName = previousFunctionName;
-        recursiveAlias = previousAlias;
 
         return body;
     }
@@ -81,12 +89,19 @@ public class IRGeneratorVisitor implements Visitor<Expression> {
     public Expression visit(IdentifierNode identifierNode) {
         var name = identifierNode.getName();
 
-        if (name.equals(currentFunctionName) && recursiveAlias != null) {
-            return new Variable(recursiveAlias);
+        if (dispatchedLambdaIds.containsKey(name)) {
+            var dispatchedLambdaId = dispatchedLambdaIds.get(name);
+
+            return new Application(new Variable(MUTUAL_DISPATCHER_NAME), new IntLiteral(dispatchedLambdaId));
+        }
+
+        if (name.equals(currentFunctionName)) {
+            return new Variable(currentFunctionName);
         }
 
         return new Variable(name);
     }
+
 
     @Override
     public Expression visit(IfNode ifNode) {
@@ -116,17 +131,53 @@ public class IRGeneratorVisitor implements Visitor<Expression> {
 
     @Override
     public Expression visit(ProgramNode programNode) {
+        var lambdaCollector = new MutuallyRecursiveLambdaCollectorVisitor();
+        programNode.accept(lambdaCollector);
+        dispatchedLambdaIds = lambdaCollector.getLambdaIds();
+
         var functionIRs = new LinkedHashMap<String, Expression>();
         for (var d : programNode.getBindings()) {
             functionIRs.put(d.getName(), d.accept(this));
         }
 
+        Expression dispatcherBody = null;
+
+        for (var e : dispatchedLambdaBodies.entrySet()) {
+            if (dispatcherBody == null) {
+                dispatcherBody = e.getValue();
+            } else {
+                var dispatchId = dispatchedLambdaIds.get(e.getKey());
+
+                var condition = new Application(
+                        new Application(new Variable("=="), new Variable(MUTUAL_DISPATCHER_TAG)),
+                        new IntLiteral(dispatchId)
+                );
+
+                dispatcherBody = new Application(
+                        new Application(
+                                new Application(new Variable("if"), condition),
+                                e.getValue()),
+                        dispatcherBody);
+            }
+        }
+
+        dispatcherBody = new Lambda(MUTUAL_DISPATCHER_TAG, dispatcherBody);
+
+        var dispatcher = new Application(Y_COMBINATOR, new Lambda(MUTUAL_DISPATCHER_NAME, dispatcherBody));
+
         Expression body = new Variable("main");
-        for (String name : functionIRs.keySet().stream().toList().reversed()) {
+        for (var name : functionIRs.keySet().stream().toList().reversed()) {
             var expr = functionIRs.get(name);
             body = new Application(new Lambda(name, body), expr);
         }
 
+        body = new Application(
+                new Lambda(MUTUAL_DISPATCHER_NAME, body),
+                dispatcher
+        );
+
         return body;
     }
+
+
 }
--- src/main/java/me/oskar/microhaskell/ir/RecursionAnalyzerVisitor.java
+++ src/main/java/me/oskar/microhaskell/ir/RecursionAnalyzerVisitor.java
@@ -3,14 +3,27 @@ package me.oskar.microhaskell.ir;
 import me.oskar.microhaskell.ast.*;
 import me.oskar.microhaskell.ast.visitor.BaseVisitor;
 
+import java.util.*;
+
 public class RecursionAnalyzerVisitor extends BaseVisitor<Void> {
 
+    private final Map<String, Set<String>> callGraph = new HashMap<>();
+    private final Map<String, FunctionDefinitionNode> nameToNode = new HashMap<>();
+
     private String currentFunctionName = null;
-    private boolean foundRecursiveCall = false;
+    private Set<String> currentFunctionCalls = new HashSet<>();
 
     @Override
-    public Void visit(AnonymousFunctionNode anonymousFunctionNode) {
-        anonymousFunctionNode.getBody().accept(this);
+    public Void visit(ProgramNode programNode) {
+        for (var binding : programNode.getBindings()) {
+            if (binding instanceof FunctionDefinitionNode fn) {
+                nameToNode.put(fn.getName(), fn);
+                fn.accept(this);
+                callGraph.put(fn.getName(), currentFunctionCalls);
+            }
+        }
+
+        detectRecursionViaSCC();
 
         return null;
     }
@@ -18,11 +31,8 @@ public class RecursionAnalyzerVisitor extends BaseVisitor<Void> {
     @Override
     public Void visit(FunctionDefinitionNode functionDefinitionNode) {
         currentFunctionName = functionDefinitionNode.getName();
-        foundRecursiveCall = false;
-
+        currentFunctionCalls = new HashSet<>();
         functionDefinitionNode.getBody().accept(this);
-
-        functionDefinitionNode.setAppliedRecursively(foundRecursiveCall);
 
         return null;
     }
@@ -37,9 +47,14 @@ public class RecursionAnalyzerVisitor extends BaseVisitor<Void> {
 
     @Override
     public Void visit(IdentifierNode identifierNode) {
-        if (identifierNode.getName().equals(currentFunctionName)) {
-            foundRecursiveCall = true;
-        }
+        currentFunctionCalls.add(identifierNode.getName());
+
+        return null;
+    }
+
+    @Override
+    public Void visit(AnonymousFunctionNode anonymousFunctionNode) {
+        anonymousFunctionNode.getBody().accept(this);
 
         return null;
     }
@@ -58,18 +73,79 @@ public class RecursionAnalyzerVisitor extends BaseVisitor<Void> {
         letNode.getExpression().accept(this);
 
         for (var b : letNode.getBindings()) {
+            nameToNode.put(b.getName(), b);
+            currentFunctionName = b.getName();
+            currentFunctionCalls = new HashSet<>();
             b.accept(this);
+            callGraph.put(currentFunctionName, currentFunctionCalls);
         }
 
         return null;
     }
 
-    @Override
-    public Void visit(ProgramNode programNode) {
-        for (var d : programNode.getBindings()) {
-            d.accept(this);
+
+    private void detectRecursionViaSCC() {
+        var indexMap = new HashMap<String, Integer>();
+        var lowLinkMap = new HashMap<String, Integer>();
+        var stack = new ArrayDeque<String>();
+        var onStack = new HashSet<String>();
+        var sccs = new ArrayList<Set<String>>();
+
+        var index = new int[]{0};
+
+        for (var function : callGraph.keySet()) {
+            if (indexMap.containsKey(function)) continue;
+
+            strongConnect(function, index, indexMap, lowLinkMap, stack, onStack, sccs);
         }
 
-        return null;
+        for (Set<String> scc : sccs) {
+            if (scc.size() > 1) {
+                for (String fn : scc) {
+                    nameToNode.get(fn).setAppliedMutuallyRecursively(true);
+                }
+            } else {
+                var fn = scc.iterator().next();
+                if (callGraph.getOrDefault(fn, Set.of()).contains(fn)) {
+                    nameToNode.get(fn).setAppliedRecursively(true);
+                }
+            }
+        }
+    }
+
+    private void strongConnect(
+            String function,
+            int[] index,
+            Map<String, Integer> indexMap,
+            Map<String, Integer> lowLinkMap,
+            Deque<String> stack,
+            Set<String> onStack,
+            List<Set<String>> sccs) {
+
+        indexMap.put(function, index[0]);
+        lowLinkMap.put(function, index[0]);
+        index[0]++;
+        stack.push(function);
+        onStack.add(function);
+
+        for (var callee : callGraph.getOrDefault(function, Set.of())) {
+            if (!indexMap.containsKey(callee)) {
+                strongConnect(callee, index, indexMap, lowLinkMap, stack, onStack, sccs);
+                lowLinkMap.put(function, Math.min(lowLinkMap.get(function), lowLinkMap.get(callee)));
+            } else if (onStack.contains(callee)) {
+                lowLinkMap.put(function, Math.min(lowLinkMap.get(function), indexMap.get(callee)));
+            }
+        }
+
+        if (lowLinkMap.get(function).equals(indexMap.get(function))) {
+            var scc = new HashSet<String>();
+            String fn;
+            do {
+                fn = stack.pop();
+                onStack.remove(fn);
+                scc.add(fn);
+            } while (!fn.equals(function));
+            sccs.add(scc);
+        }
     }
 }
