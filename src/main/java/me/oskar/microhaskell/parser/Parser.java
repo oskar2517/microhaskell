9057c040c24378864d8e6b6711680de426317b44
Oskar <12718920+oskar2517@users.noreply.github.com>
2025-05-29 21:33:03 +0200

    fix operators being atomic expressions

--- src/main/java/me/oskar/microhaskell/parser/Parser.java
+++ src/main/java/me/oskar/microhaskell/parser/Parser.java
@@ -238,14 +238,6 @@ public class Parser {
 
         return switch (currentToken.type()) {
             case IDENT -> new IdentifierNode(span, eatToken(TokenType.IDENT).lexeme());
-            case PLUS -> new IdentifierNode(span, eatToken(TokenType.PLUS).lexeme());
-            case MINUS -> new IdentifierNode(span, eatToken(TokenType.MINUS).lexeme());
-            case ASTERISK -> new IdentifierNode(span, eatToken(TokenType.ASTERISK).lexeme());
-            case SLASH -> new IdentifierNode(span, eatToken(TokenType.SLASH).lexeme());
-            case LESS_THAN -> new IdentifierNode(span, eatToken(TokenType.LESS_THAN).lexeme());
-            case LESS_THAN_EQUAL -> new IdentifierNode(span, eatToken(TokenType.LESS_THAN_EQUAL).lexeme());
-            case GREATER_THAN -> new IdentifierNode(span, eatToken(TokenType.GREATER_THAN).lexeme());
-            case GREATER_THAN_EQUAL -> new IdentifierNode(span, eatToken(TokenType.GREATER_THAN_EQUAL).lexeme());
             case INT -> new IntLiteralNode(span, Integer.parseInt(eatToken(TokenType.INT).lexeme()));
             default -> {
                 error.unexpectedToken(currentToken, "atomic expression");
